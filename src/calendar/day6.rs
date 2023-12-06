7e36293c4f837aec27f7c45622cd9a7a6c2326c2
Artur Das <a.daschevici@gmail.com>
2023-12-06 18:14:07 +0100

    Day 6

--- src/calendar/day6.rs
+++ src/calendar/day6.rs
@@ -16,13 +16,19 @@ pub fn router() -> Router {
 struct Day6Response {
     #[serde(rename = "elf")]
     elf: usize,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+struct Day6ResponseExtended {
+    #[serde(rename = "elf")]
+    elf: usize,
     #[serde(rename = "elf on a shelf")]
     elf_on_a_shelf: usize,
     #[serde(rename = "shelf with no elf on it")]
     shelf: usize,
 }
 
-async fn count_not_on_a_shelf(s: &str, substr_regex: Regex) -> usize {
+async fn count_occurences(s: &str, substr_regex: Regex) -> usize {
     substr_regex
         .captures_iter(s)
         .filter(|caps| caps.get(1).is_none())
@@ -33,10 +39,21 @@ async fn elfcount(text: String) -> Response {
     let elf_regex = Regex::new(r"elf").unwrap();
     let elf_on_a_shelf_regex = Regex::new(r"elf on a shelf").unwrap();
     let shelf_with_no_elf_regex = Regex::new(r#"(elf on a )?shelf"#).unwrap();
-    let response = Day6Response {
-        elf: count_not_on_a_shelf(&text, elf_regex).await,
-        elf_on_a_shelf: count_not_on_a_shelf(&text, elf_on_a_shelf_regex).await,
-        shelf: count_not_on_a_shelf(&text, shelf_with_no_elf_regex).await,
+    let extended = match text.find("elf on a shelf") {
+        Some(_) => {
+            let response = Day6ResponseExtended {
+                elf: count_occurences(&text, elf_regex).await,
+                elf_on_a_shelf: count_occurences(&text, elf_on_a_shelf_regex).await,
+                shelf: count_occurences(&text, shelf_with_no_elf_regex).await,
+            };
+            Json(response).into_response()
+        }
+        None => {
+            let response = Day6Response {
+                elf: count_occurences(&text, elf_regex).await,
+            };
+            Json(response).into_response()
+        }
     };
-    Json(response).into_response()
+    extended
 }
