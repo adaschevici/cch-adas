5e1ff01602f314cb0d0c8de4c55038c1ae6df704
Artur Das <a.daschevici@gmail.com>
2023-12-08 00:05:45 +0100

    Add day 7 and an alternative day6

--- src/calendar/day6.rs
+++ src/calendar/day6.rs
@@ -35,6 +35,29 @@ async fn count_occurences(s: &str, substr_regex: Regex) -> usize {
         .count()
 }
 
+async fn one_pass(text: String) -> Response {
+    let mut elves = 0usize;
+    let mut shelves = 0usize;
+    let mut elves_on_shelves = 0usize;
+
+    text.split("elf").for_each(|s| {
+        elves += 1;
+        if s.ends_with("sh") {
+            shelves += 1;
+        }
+        if s.trim() == "on a sh" {
+            elves_on_shelves += 1;
+        }
+    });
+
+    Json(Day6ResponseExtended {
+        elf: elves - 1, // Counts one extra.
+        elf_on_a_shelf: elves_on_shelves,
+        shelf: shelves - elves_on_shelves,
+    })
+    .into_response()
+}
+
 async fn elfcount(text: String) -> Response {
     let elf_regex = Regex::new(r"elf").unwrap();
     let elf_on_a_shelf_regex = Regex::new(r"elf on a shelf").unwrap();
--- src/calendar/day7.rs
+++ src/calendar/day7.rs
@@ -1,5 +1,134 @@
-use axum::Router;
+use std::collections::HashMap;
+
+use axum::{
+    async_trait,
+    extract::{FromRequestParts, Json},
+    http::{
+        header::{HeaderValue, COOKIE},
+        request::Parts,
+        StatusCode,
+    },
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use base64::{engine::general_purpose, Engine as _};
+use serde::{Deserialize, Serialize};
+use serde_json::{from_str, json};
 
 pub fn router() -> Router {
     Router::new()
+        .route("/7/decode", get(decode_recipe))
+        .route("/7/bake", get(secret_recipe))
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct RecipeDecoder {
+    flour: u64,
+    #[serde(rename = "chocolate chips")]
+    chocolate_chips: u64,
+}
+
+type Recipe = HashMap<String, u64>;
+type Pantry = HashMap<String, u64>;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Bakery {
+    recipe: Recipe,
+    pantry: Pantry,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Baked {
+    cookies: u64,
+    pantry: Pantry,
+}
+
+struct ExtractCookie(HeaderValue);
+
+#[async_trait]
+impl<S> FromRequestParts<S> for ExtractCookie
+where
+    S: Send + Sync,
+{
+    type Rejection = (StatusCode, &'static str);
+
+    async fn from_request_parts(parts: &mut Parts, _: &S) -> Result<Self, Self::Rejection> {
+        if let Some(cookie) = parts.headers.get(COOKIE) {
+            Ok(ExtractCookie(cookie.clone()))
+        } else {
+            Err((StatusCode::BAD_REQUEST, "Missing cookie"))
+        }
+    }
+}
+
+async fn decode_recipe(ExtractCookie(cookie): ExtractCookie) -> Response {
+    let recipe_base64_text = cookie.clone();
+    let recipe_details = general_purpose::STANDARD
+        .decode(
+            recipe_base64_text
+                .to_str()
+                .unwrap()
+                .split("recipe=")
+                .nth(1)
+                .unwrap()
+                .as_bytes(),
+        )
+        .unwrap();
+    let response: RecipeDecoder =
+        from_str(String::from_utf8(recipe_details).unwrap().as_str()).unwrap();
+    Json(response).into_response()
+}
+
+async fn calculate_max_cookies(recipe: &Recipe, pantry: &Pantry) -> u64 {
+    let mut max_cookies = 0;
+    for (ingredient, amount) in recipe.iter() {
+        let mut max_ingredient = 0;
+        if let Some(pantry_amount) = pantry.get(ingredient) {
+            max_ingredient = pantry_amount / amount;
+        }
+        if max_cookies == 0 || max_ingredient < max_cookies {
+            max_cookies = max_ingredient;
+        }
+    }
+    max_cookies
+}
+
+async fn leftover_in_pantry(recipe: &Recipe, pantry: &Pantry, max_cookies: u64) -> Pantry {
+    let mut leftover_pantry = pantry.clone();
+    for (ingredient, amount) in recipe.iter() {
+        if let Some(pantry_amount) = pantry.get(ingredient) {
+            leftover_pantry.insert(ingredient.clone(), pantry_amount - (amount * max_cookies));
+        }
+    }
+    leftover_pantry
+}
+
+async fn secret_recipe(ExtractCookie(cookie): ExtractCookie) -> Response {
+    let recipe_base64_text = cookie.clone();
+    let recipe_details = general_purpose::STANDARD
+        .decode(
+            recipe_base64_text
+                .to_str()
+                .unwrap()
+                .split("recipe=")
+                .nth(1)
+                .unwrap()
+                .as_bytes(),
+        )
+        .unwrap();
+    let decoded_payload: Bakery =
+        from_str(String::from_utf8(recipe_details).unwrap().as_str()).unwrap();
+    let max_cookies = calculate_max_cookies(&decoded_payload.recipe, &decoded_payload.pantry).await;
+    let leftover_pantry = leftover_in_pantry(
+        &decoded_payload.recipe,
+        &decoded_payload.pantry,
+        max_cookies,
+    )
+    .await;
+    let response = Baked {
+        cookies: max_cookies,
+        pantry: leftover_pantry,
+    };
+    Json(response).into_response()
 }
