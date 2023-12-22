1efaf756c9c89e74e9fe7ea0b24a7c918362c04c
Artur Das <a.daschevici@gmail.com>
2023-12-22 17:37:59 +0100

    Second iteration on day 19

--- src/calendar/day19.rs
+++ src/calendar/day19.rs
@@ -1,202 +1,218 @@
+#![allow(clippy::unwrap_used, clippy::significant_drop_tightening)]
+
+use anyhow::anyhow;
 use axum::{
     extract::{
-        ws::{Message, WebSocket, WebSocketUpgrade},
-        Path, State,
+        ws::{Message::Text, WebSocket},
+        Path, State, WebSocketUpgrade,
     },
-    http::StatusCode,
-    response::IntoResponse,
+    response::Response,
     routing::{get, post},
     Router,
 };
-use futures_util::{SinkExt, StreamExt};
-use serde::Deserialize;
-use serde_json::json;
+use futures_util::{sink::SinkExt, stream::StreamExt};
 use std::{
     collections::HashMap,
-    sync::{Arc, RwLock},
+    sync::{
+        atomic::{AtomicBool, AtomicU32, Ordering},
+        Arc, RwLock,
+    },
 };
-use tokio::sync::broadcast;
+use tokio::sync::broadcast::{self, Sender};
+use tracing::{info, warn};
 
-#[derive(Clone)]
-struct GameState {
-    started: bool,
+use super::AppError;
+
+pub fn router() -> Router {
+    let ping_pong_state = PingPongAppState::new();
+    let twitter_state = BirdAppState::new();
+    let ping_pong_game = Router::new()
+        .route("/ping", get(ping))
+        .with_state(ping_pong_state);
+    let tweet = Router::new()
+        .route("/views", get(views))
+        .route("/reset", post(reset))
+        .route("/ws/room/:room_id/user/:user", get(tweet))
+        .with_state(twitter_state);
+
+    Router::new().nest("/19", ping_pong_game).nest("/19", tweet)
+}
+
+#[axum::debug_handler]
+async fn ping(ws: WebSocketUpgrade, State(state): State<PingPongAppState>) -> Response {
+    ws.on_upgrade(move |socket| handle_ping(socket, state))
+}
+
+async fn handle_ping(mut socket: WebSocket, state: PingPongAppState) {
+    while let Some(msg) = socket.recv().await {
+        let Ok(msg) = msg else {
+            return;
+        };
+
+        dbg!(&msg);
+        if let Text(msg) = &msg {
+            match msg.as_str() {
+                "serve" => state.started.store(true, Ordering::Relaxed),
+                "ping" => {
+                    if state.started.load(Ordering::Relaxed) {
+                        socket
+                            .send(Text("pong".to_string()))
+                            .await
+                            .expect("Could not send message");
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
+async fn reset(State(state): State<BirdAppState>) -> Result<(), AppError> {
+    let _ = state.views.swap(0, Ordering::Relaxed);
+
+    Ok(())
+}
+
+async fn views(State(state): State<BirdAppState>) -> String {
+    state.views.load(Ordering::Relaxed).to_string()
+}
+
+type RoomId = i32;
+
+#[derive(Clone, Debug)]
+struct PingPongAppState {
+    started: Arc<AtomicBool>,
+}
+
+impl PingPongAppState {
+    fn new() -> Self {
+        Self {
+            started: Arc::new(AtomicBool::new(false)),
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
-struct TwitterState {
-    tweet_count: u64,
-    rooms: HashMap<i64, broadcast::Sender<String>>,
+struct BirdAppState {
+    views: Arc<AtomicU32>,
+    rooms: Arc<RwLock<HashMap<RoomId, RoomState>>>,
 }
 
-impl TwitterState {
+#[derive(Debug)]
+struct RoomState {
+    sender: Sender<Tweet>,
+}
+
+impl RoomState {
     fn new() -> Self {
         Self {
-            tweet_count: 0,
-            rooms: HashMap::new(),
-        }
-    }
-
-    fn reset(&mut self) {
-        self.tweet_count = 0;
-        self.rooms.clear();
-    }
-
-    fn increment(&mut self) {
-        self.tweet_count += 1;
-    }
-}
-#[derive(Clone)]
-struct FeedState {
-    tweets: Arc<RwLock<TwitterState>>,
-    conn: TweetParams,
-}
-
-#[derive(Deserialize, Clone)]
-struct TweetParams {
-    room: i64,
-    name: String,
-}
-
-pub fn router() -> Router {
-    let ping_pong_game = Router::new()
-        .route("/ws/ping", get(play_ping_pong))
-        .with_state(GameState { started: false });
-
-    let twitter = Router::new()
-        .route("/reset", post(reset_tweet_metrics))
-        .route("/views", get(get(views_count)))
-        .route("/ws/room/:id/user/:name", get(run_twitter))
-        .with_state(Arc::new(RwLock::new(TwitterState::new())));
-    Router::new()
-        .nest("/19", ping_pong_game)
-        .nest("/19", twitter)
-}
-
-async fn play_ping_pong(ws: WebSocketUpgrade, State(state): State<GameState>) -> impl IntoResponse {
-    ws.on_upgrade(move |socket| handle_ping_socket(socket, state))
-}
-
-async fn handle_ping_socket(mut socket: WebSocket, mut state: GameState) {
-    while let Some(msg) = socket.recv().await {
-        let msg = match msg {
-            Ok(msg) => msg,
-            Err(e) => {
-                eprintln!("an error occured while receiving a message: {}", e);
-                break;
-            }
-        };
-
-        if !state.started {
-            if msg != Message::Text("serve".to_string()) {
-                eprintln!("ignoring message, got: {:?}", msg);
-                continue;
-            }
-
-            state.started = true;
-            continue;
-        }
-
-        let msg = match msg {
-            Message::Text(msg) => msg,
-            _ => {
-                eprintln!("expected text message, got: {:?}", msg);
-                break;
-            }
-        };
-
-        let msg = match msg.as_str() {
-            "ping" => "pong".to_string(),
-            _ => {
-                eprintln!("expected 'ping' got: {:?}", msg);
-                continue;
-            }
-        };
-
-        if let Err(e) = socket.send(Message::Text(msg)).await {
-            eprintln!("an error occured while sending a message: {}", e);
-            break;
+            sender: broadcast::channel(100).0,
         }
     }
 }
 
-async fn reset_tweet_metrics(State(twitter): State<Arc<RwLock<TwitterState>>>) -> StatusCode {
-    twitter
-        .write()
-        .expect("Could not acquire write lock")
-        .reset();
-    StatusCode::OK
+impl BirdAppState {
+    fn new() -> Self {
+        Self {
+            views: Arc::new(AtomicU32::new(0)),
+            rooms: Arc::new(RwLock::new(HashMap::new())),
+        }
+    }
 }
 
-async fn views_count(State(twitter): State<Arc<RwLock<TwitterState>>>) -> String {
-    let twitter = twitter.read().unwrap();
-    dbg!(&twitter);
-    format!("{}", twitter.tweet_count)
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+struct TweetInput {
+    message: String,
 }
 
-async fn run_twitter(
-    ws: WebSocketUpgrade,
-    Path((id, name)): Path<(i64, String)>,
-    State(twitter): State<Arc<RwLock<TwitterState>>>,
-) -> impl IntoResponse {
-    ws.on_upgrade(move |socket| {
-        handle_twitter_socket(
-            socket,
-            FeedState {
-                tweets: twitter,
-                conn: TweetParams { room: id, name },
-            },
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+struct Tweet {
+    user: String,
+    message: TweetInput,
+}
+
+impl TryFrom<&String> for TweetInput {
+    type Error = AppError;
+
+    fn try_from(value: &String) -> Result<Self, Self::Error> {
+        let tweet_input = serde_json::from_str::<Self>(value)
+            .map_err(|e| anyhow!("Error parsing TweetInput: {}", e))?;
+
+        if tweet_input.message.len() > 128 {
+            return Err(anyhow!("Message length cannot be over 128").into());
+        }
+
+        Ok(tweet_input)
+    }
+}
+
+impl From<Tweet> for String {
+    fn from(value: Tweet) -> Self {
+        format!(
+            r#"{{
+      "user": "{}",
+      "message": "{}"
+    }}"#,
+            value.user, value.message.message
         )
-    })
+    }
 }
 
-async fn handle_twitter_socket(socket: WebSocket, state: FeedState) {
-    #[derive(Deserialize)]
-    struct Msg {
-        message: String,
+async fn tweet(
+    ws: WebSocketUpgrade,
+    Path((room, user)): Path<(i32, String)>,
+    State(state): State<BirdAppState>,
+) -> Response {
+    ws.on_upgrade(move |c| handle_tweet(c, room, user, Arc::new(state)))
+}
+
+async fn handle_tweet(socket: WebSocket, room: i32, user: String, state: Arc<BirdAppState>) {
+    let (mut sender, mut receiver) = socket.split();
+    #[allow(unused_assignments)]
+    let mut room_sender = None::<Sender<Tweet>>;
+
+    if let Some(room_state) = state.rooms.read().unwrap().get(&room) {
+        room_sender = Some(room_state.sender.clone());
     }
 
-    let tweets = state.tweets;
+    if room_sender.is_none() {
+        let mut rooms = state.rooms.write().unwrap();
+        let room_state = rooms.entry(room).or_insert_with(RoomState::new);
+        room_sender = Some(room_state.sender.clone());
+    }
 
-    let (mut send, mut recv) = socket.split();
+    let room_sender = room_sender.unwrap();
+    let mut room_receiver = room_sender.subscribe();
 
-    let out = tweets
-        .write()
-        .expect("could not establish write lock on data")
-        .rooms
-        .entry(state.conn.room)
-        .or_insert(broadcast::channel(100).0)
-        .clone();
+    let mut send = tokio::spawn(async move {
+        while let Some(msg) = receiver.next().await {
+            let Ok(msg) = msg else {
+                return;
+            };
 
-    let mut sub = out.subscribe();
-
-    let mut send_tweet = tokio::spawn(async move {
-        while let Ok(msg) = sub.recv().await {
-            tweets
-                .write()
-                .expect("could not establish write lock on data")
-                .increment();
-            if send.send(Message::Text(msg)).await.is_err() {
-                break;
+            if let Text(text) = &msg {
+                match TweetInput::try_from(text) {
+                    Ok(message) => {
+                        info!("Parsed {:?}", message);
+                        let user = user.clone();
+                        let _ = room_sender.send(Tweet { user, message }).unwrap();
+                    }
+                    Err(e) => warn!("Failed to parse TweetInput: {:?}", e),
+                }
             }
         }
     });
 
-    let mut recv_tweet = {
-        tokio::spawn(async move {
-            while let Some(Ok(Message::Text(msg))) = recv.next().await {
-                let msg: Msg = serde_json::from_str(msg.as_str())
-                    .expect("could not deserialize input message");
-                if msg.message.chars().count() > 128 {
-                    continue;
-                }
-                let msg = json!({"user":state.conn.name.clone(),"message":msg.message}).to_string();
-                let _ = out.send(msg);
-            }
-        })
-    };
+    let mut receive = tokio::spawn(async move {
+        while let Ok(msg) = room_receiver.recv().await {
+            let msg_str: String = msg.into();
+            let _ = state.views.fetch_add(1, Ordering::Relaxed);
+            sender.send(Text(msg_str)).await.unwrap();
+        }
+    });
 
     tokio::select! {
-        _ = (&mut send_tweet) => recv_tweet.abort(),
-        _ = (&mut recv_tweet) => send_tweet.abort(),
+        _ = (&mut send) => receive.abort(),
+        _ = (&mut receive) => send.abort(),
     };
 }
--- src/calendar/mod.rs
+++ src/calendar/mod.rs
@@ -1,4 +1,4 @@
-use axum::Router;
+use axum::{http::StatusCode, response::IntoResponse, Router};
 use sqlx::PgPool;
 
 mod day0;
@@ -24,6 +24,25 @@ mod day6;
 mod day7;
 mod day8;
 mod day9;
+
+#[derive(Debug)]
+pub struct AppError(anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response()
+    }
+}
+
+impl<E: Into<anyhow::Error>> From<E> for AppError {
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
+}
 
 pub(crate) fn router(pool: PgPool) -> Router {
     Router::new()
